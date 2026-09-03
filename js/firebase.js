5a37ec6d2bf18e656a1cafabe40395de53005a78
ArthurWillekens <arthurwillekens@gmail.com>
2026-09-03 16:26:44 +0200

    Save local CSS and Firebase fix

--- js/firebase.js
+++ js/firebase.js
@@ -41,7 +41,7 @@ import {
 
 const firebaseConfig = {
 
-    apiKey: "AIzaSyBIvAp40mYi9mB2P7CQG8Lw5xQ8m85hE",
+    apiKey: "AIzaSyBIvAp40mYiAi9mB2P7CQG8Lw5xQ8m85hE",
 
     authDomain:
         "teckelweb.firebaseapp.com",
